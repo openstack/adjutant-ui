58504b599f73194ab0b0f296ded526eb2394fc7d
Kevin Masterson <kmmasterson@gmail.com>
2021-02-15 12:45:37 -0500

    Now checks for code 202 when checking for success for other operations.
    Task: 41574
    Story: 2008505
    Change-Id: I58ea7dc0aae66a38a00ed68e6c6b2cd9a4cbd32b
    (cherry picked from commit 104c474479e9a89d22b96777378ff59983c03393)

--- adjutant_ui/content/notifications/tables.py
+++ adjutant_ui/content/notifications/tables.py
@@ -44,7 +44,7 @@ class AcknowlegeNotifcation(tables.BatchAction):
 
     def action(self, request, obj_id):
         result = adjutant.notifications_acknowlege(request, obj_id)
-        if not result or result.status_code != 200:
+        if not result or result.status_code not in [200, 202]:
             exception = exceptions.NotAvailable()
             exception._safe_message = False
             raise exception
--- adjutant_ui/content/quota/tables.py
+++ adjutant_ui/content/quota/tables.py
@@ -77,7 +77,7 @@ class CancelQuotaTask(tables.DeleteAction):
 
     def delete(self, request, obj_id):
         result = adjutant.task_cancel(request, obj_id)
-        if not result or result.status_code != 200:
+        if not result or result.status_code not in [200, 202]:
             exception = exceptions.NotAvailable()
             exception._safe_message = False
             raise exception
